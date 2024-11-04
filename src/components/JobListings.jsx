aac8e7baab3348d03c16ab998cdb3c16e9b74c2d
ThuRa Lynn Htun <lynnhtunthura8@gmail.com>
2024-11-04 15:04:58 +0630

    Fix the error of jobs being not an array

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -15,7 +15,7 @@ const JobListings = ({ isHome = false }) => {
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
-        setJobs(data);
+        setJobs(data.jobs);
       } catch (error) {
         console.log("Error fetching data ", error);
       } finally {
