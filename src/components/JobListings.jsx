62ee61e2941abec45acd48a3b9e536f112e1332f
ThuRa Lynn Htun <lynnhtunthura8@gmail.com>
2024-11-04 15:00:09 +0630

    Display three jobs on home , all jobs otherwise

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -9,7 +9,8 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+      // const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+      const apiUrl = "/jobs.json";
 
       try {
         const res = await fetch(apiUrl);
@@ -25,6 +26,8 @@ const JobListings = ({ isHome = false }) => {
     fetchJobs();
   }, []);
 
+  const displayedJobs = isHome ? jobs.slice(0, 3) : jobs;
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -37,7 +40,7 @@ const JobListings = ({ isHome = false }) => {
         ) : (
           <>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {jobs.map((job) => (
+              {displayedJobs.map((job) => (
                 <JobListing key={job.id} job={job} />
               ))}
             </div>
